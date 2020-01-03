49f12e8793787b41ef2b5723c7bde33750c45342
Emil Sharafutdinov <emil-shar@yandex.ru>
2020-01-03 13:07:52 +0300

    Update Day1.scala

--- Day1.scala
+++ Day1.scala
@@ -4,10 +4,9 @@ def getCodeFirst(input: String): Int = {
   val listForSum = ArrayBuffer[Int]()
   for {
     i <- 0 until input.length - 1
+    if input.charAt(i) == input.charAt(i + 1)
   } {
-    if (input.charAt(i) == input.charAt(i + 1)) {
-      listForSum += input.charAt(i).toString.toInt
-    }
+    listForSum += input.charAt(i).toString.toInt
   }
   if (input.charAt(input.length - 1) == input.charAt(0)) {
     listForSum += input.charAt(input.length - 1).toString.toInt
@@ -17,4 +16,12 @@ def getCodeFirst(input: String): Int = {
 
 //Second star
 def getCodeSecond(input: String): Int = {
+  val stepsForward = input.length / 2
+  val list = for {
+    i <- 0 until stepsForward
+    if input.charAt(i) == input.charAt(i + stepsForward)
+  } yield {
+    input.charAt(i).toString.toInt * 2
+  }
+  list.sum
 }
